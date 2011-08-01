6014f10c58162983b2af7554c07f3a0aea6888aa
Jimmy Bergman <jimmy@atomia.com>
2011-08-01 16:17:35 +0200

    Remove svn helpers used for other non-public projects

--- deploy-apt.sh
+++ deploy-apt.sh
@@ -5,8 +5,5 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 	exit 1
 fi
 
-svn add packages/"$2"/*"$1"*
-svn commit -m "Release $2 apt-packages for version $1"
-
 scp packages/"$2"/*"$1"*.deb root@apt.atomia.com:
 ssh root@apt.atomia.com 'cd /var/packages/ubuntu-'"$2"' && reprepro includedeb '"$2"' /root/libatomia-bindings-coreapi-perl_*'"$1"'*.deb'
